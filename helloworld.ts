bd6555108d51af7bdb482584a6cb6672d21199ed
seunghyeokleeme <seunghyeok.tech@gmail.com>
2020-03-19 01:09:42 +0900

    deno helloworld

---
+++ helloworld.ts
@@ -0,0 +1,8 @@
+import { serve } from "https://deno.land/std@v0.36.0/http/server.ts";
+const s = serve({ port: 8000 });
+console.log("http://localhost:8000/");
+for await (const req of s) {
+  req.respond({ body: "Hello World\n" });
+}
+
+// 간단한 hello world 예제
